195411dc58bb464ee9ac01b51ee145e3659cb54a
Jason Buckner <jasonb@archive.org>
2021-03-30 13:25:58 -0700

    Update the demo to use an <app-root> component (#3)
    This makes it easier to create module demos since you can use Typescript all the way down.

--- src/your-webcomponent.ts
+++ src/your-webcomponent.ts
@@ -2,6 +2,22 @@ import { html, css, LitElement, property, customElement } from 'lit-element';
 
 @customElement('your-webcomponent')
 export class YourWebComponent extends LitElement {
+  @property({ type: String }) title = 'Hey there';
+
+  @property({ type: Number }) counter = 5;
+
+  private increment() {
+    this.counter += 1;
+  }
+
+  render() {
+    return html`
+      <h2>${this.title}, Number: ${this.counter}!</h2>
+      <button @click=${this.increment}>increment</button>
+      <slot name="my-slot"> </slot>
+    `;
+  }
+
   static styles = css`
     :host {
       display: block;
@@ -9,20 +25,4 @@ export class YourWebComponent extends LitElement {
       color: var(--your-webcomponent-text-color, #000);
     }
   `;
-
-  @property({ type: String }) title = 'Hey there';
-
-  @property({ type: Number }) counter = 5;
-
-  __increment() {
-    this.counter += 1;
-  }
-
-  render() {
-    return html`
-      <h2>${this.title}, Number: ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
-      <slot name="my-slot"> </slot>
-    `;
-  }
 }
