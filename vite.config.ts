e97687a059f83cd8d5d55f879159f31863951f81
clau <1281581+dualcnhq@users.noreply.github.com>
2022-07-12 04:45:07 +0800

    Update vite config for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,25 @@
-import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
-  publicDir: 'demo',
+  base: './',
+  root: resolve(__dirname, './demo'),
   build: {
-    outDir: 'ghpages',
+    /**
+     * This is the directory where the built files will be placed
+     * that we upload to GitHub Pages.
+     */
+    outDir: '../ghpages/demo',
+    emptyOutDir: true,
     manifest: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'demo/index.html'),
       },
+      output: {
+        entryFileNames: 'app-root.js',
+      },
     },
-  },
-  server: {
-    host: true,
-    port: 8080,
-    open: true,
-    cors: true,
   },
 });
